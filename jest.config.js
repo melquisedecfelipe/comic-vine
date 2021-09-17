e90ff358db4ffff1ef79e3fd38ef80bf6a175715
Melquisedec Felipe <melquisedecfelipe@gmail.com>
2021-09-17 16:58:51 -0300

    feat: add initial config

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?',
+    '!src/**/stories.tsx',
+    '!src/**/styles.ts',
+    '!src/pages/**/*.tsx',
+    '!src/styles/**/*.ts',
+    '!src/types/**/*.d.ts',
+    '!src/**/mock.ts'
+  ],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  modulePaths: ['<rootDir>/src/', '<rootDir>/.jest']
+}
