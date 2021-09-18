547b906d4e883263a97ed500b34bd5d1ff8c6fcb
Melquisedec Felipe <melquisedecfelipe@gmail.com>
2021-09-18 15:13:08 -0300

    styles: add theme styles

---
+++ src/styles/global.ts
@@ -0,0 +1,64 @@
+import {
+  createGlobalStyle,
+  css,
+  DefaultTheme,
+  GlobalStyleComponent
+} from 'styled-components'
+
+const GlobalStyles: GlobalStyleComponent<DefaultTheme> = createGlobalStyle`
+  @font-face {
+    font-family: 'WorkSans';
+    font-style: normal;
+    font-weight: 400;
+    font-display: swap;
+    src: local('WorkSans Regular'), local('WorkSans-Regular'),
+        url('/fonts/WorkSans-Regular.woff2') format('woff2');
+  }
+
+  @font-face {
+    font-family: 'WorkSans';
+    font-style: normal;
+    font-weight: 500;
+    font-display: swap;
+    src: local('WorkSans Medium'), local('WorkSans-Medium'),
+        url('/fonts/WorkSans-Medium.woff2') format('woff2');
+  }
+
+  @font-face {
+    font-family: 'Barlow';
+    font-style: normal;
+    font-weight: 600;
+    font-display: swap;
+    src: local('Barlow SemiBold'), local('Barlow-SemiBold'),
+        url('/fonts/Barlow-SemiBold.woff2') format('woff2');
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    scroll-behavior: smooth;
+
+    &::before,
+    &::after {
+      box-sizing: inherit;
+    }
+  }
+
+  ${({ theme }) => css`
+    body {
+      font-family: ${theme.font.familyWorkSans};
+      font-size: ${theme.font.sizes.medium};
+
+      a {
+        font-weight: ${theme.font.medium};
+        color: ${theme.colors.link};
+      }
+    }
+  `}
+
+`
+
+export default GlobalStyles
