715d20e9e2e5d18d094616c7193dd4ae2a5550c4
Melquisedec Felipe <melquisedecfelipe@gmail.com>
2021-09-20 17:57:44 -0300

    feat: add next progress and provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,12 @@
 import { AppProps } from 'next/app'
 import Head from 'next/head'
+import NextNprogress from 'nextjs-progressbar'
 
 import { QueryClient, QueryClientProvider } from 'react-query'
 
 import { ThemeProvider } from 'styled-components'
+
+import { CharactersProvider } from 'hooks/useCharacters'
 
 import GlobalStyles from 'styles/global'
 import theme from 'styles/theme'
@@ -14,18 +17,26 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
-        <Head>
-          <title>Comic Vine</title>
-          <link rel="shortcut icon" href="/img/icon-512.png" />
-          <link rel="apple-touch-icon" href="/img/icon-512.png" />
-          <link rel="manifest" href="/manifest.json" />
-          <meta
-            name="description"
-            content="Comic Vine is the largest comic book wiki in the universe"
+        <CharactersProvider>
+          <Head>
+            <title>Comic Vine</title>
+            <link rel="shortcut icon" href="/img/icon-512.png" />
+            <link rel="apple-touch-icon" href="/img/icon-512.png" />
+            <link rel="manifest" href="/manifest.json" />
+            <meta
+              name="description"
+              content="Comic Vine is the largest comic book wiki in the universe"
+            />
+          </Head>
+          <GlobalStyles />
+          <NextNprogress
+            color="#000000"
+            startPosition={0.3}
+            stopDelayMs={200}
+            height={4}
           />
-        </Head>
-        <GlobalStyles />
-        <Component {...pageProps} />
+          <Component {...pageProps} />
+        </CharactersProvider>
       </ThemeProvider>
     </QueryClientProvider>
   )
