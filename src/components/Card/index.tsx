8989b916b8bc4d66658674acc5d0cb98b6de6c49
Melquisedec Felipe <melquisedecfelipe@gmail.com>
2021-09-21 09:11:20 -0300

    feat: update card component

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,10 +1,7 @@
 import { useRouter } from 'next/dist/client/router'
 import Image from 'next/image'
 
-import { Heart as HeartIcon } from '@styled-icons/boxicons-regular/Heart'
-import { Heart as HeartFillIcon } from '@styled-icons/boxicons-solid/Heart'
-
-import Button from 'components/Button'
+import FavoriteButton from 'components/FavoriteButton'
 
 import * as S from './styles'
 
@@ -12,19 +9,10 @@ import { Character } from 'types/characters'
 
 export type CardProps = {
   character: Character
-  handleAddFavorite: (character: Character) => void
-  handleRemoveFavorite: (id: number) => void
-  isFavorite: (id: number) => boolean
   isVertical?: boolean
 }
 
-const Card = ({
-  character,
-  handleAddFavorite,
-  handleRemoveFavorite,
-  isFavorite,
-  isVertical = false
-}: CardProps) => {
+const Card = ({ character, isVertical = false }: CardProps) => {
   const { push } = useRouter()
 
   return (
@@ -39,21 +27,7 @@ const Card = ({
           objectFit="cover"
         />
 
-        {isFavorite(character.id) ? (
-          <Button
-            aria-label="Remove Favorite"
-            onClick={() => handleRemoveFavorite(character.id)}
-          >
-            <HeartFillIcon />
-          </Button>
-        ) : (
-          <Button
-            aria-label="Add Favorite"
-            onClick={() => handleAddFavorite(character)}
-          >
-            <HeartIcon />
-          </Button>
-        )}
+        <FavoriteButton character={character} />
       </S.ImageWrapper>
 
       <S.Section
--- src/components/Card/test.tsx
+++ src/components/Card/test.tsx
@@ -2,37 +2,27 @@ import userEvent from '@testing-library/user-event'
 
 import { render, screen } from 'utils/testUtils'
 
-import Card, { CardProps } from '.'
+import Card from '.'
 import mock from './mock'
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const useRouter = jest.spyOn(require('next/router'), 'useRouter')
 
-const isFavorite = () => false
-const handleAddFavorite = jest.fn()
-const handleRemoveFavorite = jest.fn()
 const push = jest.fn()
 
 useRouter.mockImplementation(() => ({
   push
 }))
 
-const props = {
-  character: mock.character,
-  isFavorite,
-  handleAddFavorite,
-  handleRemoveFavorite
-} as CardProps
-
 describe('<Card />', () => {
   it('should render the card', () => {
-    const { container } = render(<Card {...props} />)
+    const { container } = render(<Card {...mock} />)
 
     expect(container).toMatchSnapshot()
   })
 
   it('should render the card vertical', () => {
-    render(<Card {...props} isVertical />)
+    render(<Card {...mock} isVertical />)
 
     expect(
       screen.getByRole('link', { name: /character/i }).parentElement
@@ -43,26 +33,10 @@ describe('<Card />', () => {
   })
 
   it('should redirect on card click', () => {
-    render(<Card {...props} />)
+    render(<Card {...mock} />)
 
     userEvent.click(screen.getByRole('link', { name: /character/i }))
 
     expect(push).toHaveBeenCalledTimes(1)
-  })
-
-  it('should favorite character on button click', () => {
-    render(<Card {...props} />)
-
-    userEvent.click(screen.getByRole('button', { name: /add favorite/i }))
-
-    expect(handleAddFavorite).toHaveBeenCalledTimes(1)
-  })
-
-  it('should remove favorite character on button click', () => {
-    render(<Card {...props} isFavorite={() => true} />)
-
-    userEvent.click(screen.getByRole('button', { name: /remove favorite/i }))
-
-    expect(handleRemoveFavorite).toHaveBeenCalledTimes(1)
   })
 })
