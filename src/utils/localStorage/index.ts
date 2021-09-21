2cbd12b2da28c7c9fbf4b3ba60d1ea7f6a6ea0bc
Melquisedec Felipe <melquisedecfelipe@gmail.com>
2021-09-20 21:08:34 -0300

    feat: add local storage util

---
+++ src/utils/localStorage/index.ts
@@ -0,0 +1,17 @@
+import { Character } from 'types/characters'
+
+export function getStorageItem(key: string) {
+  if (typeof window === 'undefined') return
+
+  const data = window.localStorage.getItem(key)
+
+  return JSON.parse(data!)
+}
+
+export function setStorageItem(key: string, value: string[] | Character[]) {
+  if (typeof window === 'undefined') return
+
+  const data = JSON.stringify(value)
+
+  return window.localStorage.setItem(key, data)
+}
---
+++ src/utils/localStorage/test.ts
@@ -0,0 +1,28 @@
+import { getStorageItem, setStorageItem } from '.'
+
+describe('getStorageItem()', () => {
+  beforeEach(() => {
+    window.localStorage.clear()
+  })
+  it('should return the item from localStorage', () => {
+    window.localStorage.setItem(
+      '@Test:localStorage',
+      JSON.stringify(['1', '2'])
+    )
+
+    expect(getStorageItem('@Test:localStorage')).toStrictEqual(['1', '2'])
+  })
+})
+
+describe('setStorageItem()', () => {
+  beforeEach(() => {
+    window.localStorage.clear()
+  })
+  it('should add the item to localStorage', () => {
+    setStorageItem('@Test:localStorage', ['1', '2'])
+
+    expect(window.localStorage.getItem('@Test:localStorage')).toStrictEqual(
+      JSON.stringify(['1', '2'])
+    )
+  })
+})
