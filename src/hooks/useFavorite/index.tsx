695e2b5377cb778d218e3db5b0244c7fecb87c32
Melquisedec Felipe <melquisedecfelipe@gmail.com>
2021-09-21 09:09:15 -0300

    feat: add use favorite hook

---
+++ src/hooks/useFavorite/index.tsx
@@ -0,0 +1,101 @@
+import {
+  useContext,
+  createContext,
+  useState,
+  useEffect,
+  useCallback
+} from 'react'
+
+import { getStorageItem, setStorageItem } from 'utils/localStorage'
+
+import { FAVORITE_STORAGE } from 'config/constants'
+
+import { Character } from 'types/characters'
+
+export type FavoriteContextData = {
+  favorites: Character[]
+  handleAddFavorite: (character: Character) => void
+  handleRemoveFavorite: (id: number) => void
+  isFavorite: (id: number) => boolean
+}
+
+export const FavoriteContextDefaultValues = {
+  favorites: [],
+  handleAddFavorite: () => null,
+  handleRemoveFavorite: () => null,
+  isFavorite: () => false
+} as FavoriteContextData
+
+export const FavoriteContext = createContext<FavoriteContextData>(
+  FavoriteContextDefaultValues
+)
+
+export type FavoriteProviderProps = {
+  children: React.ReactNode
+}
+
+const FavoriteProvider = ({ children }: FavoriteProviderProps) => {
+  const [favorites, setFavorites] = useState<Character[]>([])
+
+  const handleAddFavorite = useCallback(
+    (character: Character) => {
+      handleSave([...favorites, character])
+    },
+    [favorites]
+  )
+
+  const handleRemoveFavorite = useCallback(
+    (id: number) => {
+      const newFavorites = favorites.filter(character => character.id !== id)
+
+      handleSave(newFavorites)
+    },
+    [favorites]
+  )
+
+  const handleSave = (characters: Character[]) => {
+    setFavorites(characters)
+
+    setStorageItem(FAVORITE_STORAGE, characters)
+  }
+
+  const isFavorite = useCallback(
+    (id: number) => {
+      if (id) {
+        const favoriteFilter = favorites.filter(
+          charactrer => charactrer.id === id
+        )
+
+        return !!favoriteFilter.length
+      }
+
+      return false
+    },
+    [favorites]
+  )
+
+  useEffect(() => {
+    const favoritesStorage = getStorageItem(FAVORITE_STORAGE)
+
+    if (favoritesStorage) {
+      setFavorites(favoritesStorage)
+    }
+  }, [])
+
+  return (
+    <FavoriteContext.Provider
+      value={{
+        favorites,
+        handleAddFavorite,
+        handleRemoveFavorite,
+        isFavorite
+      }}
+    >
+      {children}
+    </FavoriteContext.Provider>
+  )
+}
+
+const useFavorite = () => useContext(FavoriteContext)
+
+export { FavoriteProvider, useFavorite }
---
+++ src/hooks/useFavorite/test.tsx
@@ -0,0 +1,79 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+import { act, renderHook } from '@testing-library/react-hooks'
+
+import { Character } from 'types/characters'
+
+import { FavoriteProvider, FavoriteProviderProps, useFavorite } from '.'
+import mock from './mock'
+
+const wrapper = ({ children }: FavoriteProviderProps) => (
+  <FavoriteProvider>{children}</FavoriteProvider>
+)
+
+describe('useFavorite', () => {
+  beforeEach(() => {
+    window.localStorage.clear()
+  })
+
+  it('should return favorites characters', async () => {
+    const { result } = renderHook(() => useFavorite(), {
+      wrapper
+    })
+
+    expect(result.current.favorites).toStrictEqual([])
+  })
+
+  it('should add character in favorites', async () => {
+    const { result } = renderHook(() => useFavorite(), {
+      wrapper
+    })
+
+    act(() => {
+      result.current.handleAddFavorite(mock[0])
+    })
+
+    expect(result.current.favorites).toStrictEqual([mock[0]])
+  })
+
+  it('should remove character in favorites', async () => {
+    const { result } = renderHook(() => useFavorite(), {
+      wrapper
+    })
+
+    act(() => {
+      result.current.handleAddFavorite(mock[0])
+    })
+
+    act(() => {
+      result.current.handleAddFavorite(mock[1])
+    })
+
+    expect(result.current.favorites).toStrictEqual([mock[0], mock[1]])
+
+    act(() => {
+      result.current.handleRemoveFavorite(mock[0].id)
+    })
+
+    expect(result.current.favorites).toStrictEqual([mock[1]])
+  })
+
+  it('should character is a favorite', async () => {
+    const { result } = renderHook(() => useFavorite(), {
+      wrapper
+    })
+
+    act(() => {
+      result.current.handleAddFavorite(mock[0])
+    })
+
+    expect(result.current.isFavorite(mock[0].id)).toBe(true)
+  })
+
+  it('should character is not a favorite', async () => {
+    const { result } = renderHook(() => useFavorite(), {
+      wrapper
+    })
+
+    expect(result.current.isFavorite(mock[0].id)).toBe(false)
+  })
+})
