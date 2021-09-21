c6ada4390fb3caa0f93802773dc61a378209425b
Melquisedec Felipe <melquisedecfelipe@gmail.com>
2021-09-21 09:11:45 -0300

    feat: update constants

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,6 +1,6 @@
 export const APP_KEY = '@Méliuz'
 
-export const CHARACTERS_STORAGE = `${APP_KEY}:charactersItems`
+export const FAVORITE_STORAGE = `${APP_KEY}:favoriteCharacters`
 
 export const QUERY_CHARACTER = `${APP_KEY}:character`
 export const QUERY_CHARACTERS = `${APP_KEY}:characters`
