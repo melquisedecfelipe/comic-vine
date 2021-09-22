11313fd987cd7e59645cdcbe57c314e93b23794f
Melquisedec Felipe <melquisedecfelipe@gmail.com>
2021-09-22 13:46:19 -0300

    fix: update scroll buttons responsive

--- src/components/ScrollButtons/index.tsx
+++ src/components/ScrollButtons/index.tsx
@@ -1,3 +1,6 @@
+import { KeyboardArrowDown as ArrowDownIcon } from '@styled-icons/material-rounded/KeyboardArrowDown'
+import { KeyboardArrowUp as ArrowUpIcon } from '@styled-icons/material-rounded/KeyboardArrowUp'
+
 import Button from 'components/Button'
 
 import * as S from './styles'
@@ -10,10 +13,10 @@ export type ScrollButtonsProps = {
 const ScrollButtons = ({ handleDown, handleUp }: ScrollButtonsProps) => (
   <S.Wrapper>
     <Button aria-label="Down" variant="outlined" onClick={handleDown}>
-      Down
+      <ArrowDownIcon />
     </Button>
     <Button aria-label="Up" variant="outlined" onClick={handleUp}>
-      Up
+      <ArrowUpIcon />
     </Button>
   </S.Wrapper>
 )
--- src/components/ScrollButtons/styles.ts
+++ src/components/ScrollButtons/styles.ts
@@ -1,3 +1,5 @@
+import media from 'styled-media-query'
+
 import styled, { css } from 'styled-components'
 
 export const Wrapper = styled.div`
@@ -6,7 +8,12 @@ export const Wrapper = styled.div`
     bottom: 0;
     right: 0;
     display: flex;
+    flex-direction: column-reverse;
     gap: ${theme.spacings.medium};
     padding: ${theme.spacings.large};
+
+    ${media.greaterThan('medium')`
+      flex-direction: row;
+    `}
   `}
 `
