f7d4763105e732ac62571389184fec77733d9bf6
Melquisedec Felipe <melquisedecfelipe@gmail.com>
2021-09-22 12:57:04 -0300

    fix: update props in test

--- src/components/TextContent/test.tsx
+++ src/components/TextContent/test.tsx
@@ -2,11 +2,15 @@ import { render } from 'utils/testUtils'
 
 import mock from './mock'
 
-import TextContent from '.'
+import TextContent, { TextContentProps } from '.'
+
+const props = {
+  content: mock
+} as TextContentProps
 
 describe('<TextContent />', () => {
   it('should render the text content', () => {
-    const { container } = render(<TextContent {...mock} />)
+    const { container } = render(<TextContent {...props} />)
 
     expect(container).toBeInTheDocument()
   })
