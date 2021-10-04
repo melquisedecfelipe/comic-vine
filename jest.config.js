0c67c929ca61213d35f9f28eead17e2469f9e835
Melquisedec Felipe <melquisedecfelipe@gmail.com>
2021-10-04 11:05:25 -0300

    feat: update jest collect coverage config

--- jest.config.js
+++ jest.config.js
@@ -7,12 +7,12 @@ module.exports = {
     '!src/**/stories.tsx',
     '!src/**/styles.ts',
     '!src/config/**/*.ts',
-    '!src/pages/**/*.ts',
-    '!src/pages/**/*.tsx',
+    '!src/mock/**',
+    '!src/**/mock.ts',
+    '!src/pages/**/*.ts(x)?',
     '!src/services/**/*.ts',
-    '!src/styles/**/*.ts',
-    '!src/types/**/*.d.ts',
-    '!src/**/mock.ts'
+    '!src/styles/**',
+    '!src/types/**'
   ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   modulePaths: ['<rootDir>/src/', '<rootDir>/.jest']
